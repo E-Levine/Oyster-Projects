92f74b92d407d82d182fdd785990c836c3609823
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-01-29 06:56:27 -0500

    Update PBC_2025_presentation.R
    TripInfo, SampleEvent data selection code.

--- Data Summaries/PBC 2025/PBC_2025_presentation.R
+++ Data Summaries/PBC 2025/PBC_2025_presentation.R
@@ -20,7 +20,10 @@ Server = "localhost\\ERICALOCALSQL" #Set the local Server to use
 Estuaries <- c("LW")
 ##Data types of interest: Recruitment, Collection/Dissection. Sediment Trap, Semi-Annual Survey, Cage Growth and Mortality, Survey, Shell Budget, Wave
 DataTypes <- c("Recruitment", "Collection/Dissection", "Sediment Trap")
-#Collecting all data for site.
+DataTypeCodes <- c("RCRT", "COLL", "SDTP")
+#Dates of interest
+Start_date <- as.Date("2000-01-01")
+End_date <- as.Date("2024-12-31")
 #
 ###Database download####
 # Connect to Local database server and pull all necessary data, then close connection 
@@ -32,11 +35,11 @@ con <- dbConnect(odbc(),
 #
 FixedLocations <- tbl(con,in_schema("dbo", "FixedLocations")) %>%  collect() %>% filter(Estuary %in% Estuaries)
 #
-hsdbTripInfo <- tbl(con,in_schema("hsdb", "TripInfo")) %>%  collect() 
-dboTripInfo <- tbl(con,in_schema("dbo", "TripInfo")) %>%  collect() 
+hsdbTripInfo <- tbl(con,in_schema("hsdb", "TripInfo")) %>%  collect() %>% filter(TripDate > Start_date & TripDate < End_date)
+dboTripInfo <- tbl(con,in_schema("dbo", "TripInfo")) %>%  collect() %>% filter(TripDate > Start_date & TripDate < End_date)
 #
-hsdbSampleEvent <- tbl(con,in_schema("hsdb", "SampleEvent")) %>%   collect()
-dboSampleEvent <- tbl(con,in_schema("dbo", "SampleEvent")) %>%   collect()
+hsdbSampleEvent <- tbl(con,in_schema("hsdb", "SampleEvent")) %>%   collect() 
+dboSampleEvent <- tbl(con,in_schema("dbo", "SampleEvent")) %>%   collect() 
 #
 hsdbSampleEventWQ <- tbl(con,in_schema("hsdb", "SampleEventWQ")) %>%   collect()
 dboSampleEventWQ <- tbl(con,in_schema("dbo", "SampleEventWQ")) %>%   collect()
@@ -49,7 +52,15 @@ DBI::dbDisconnect(con)
 #
 ####Data filtering/cleaning####
 #
-TripInfo 
-hsdbTripInfo %>% 
-  filter(substring(TripID,1,2) %in% Estuaries)  %>%  arrange(TripID)
-         
+(TripInfo <- rbind(hsdbTripInfo %>% filter(substring(TripID,1,2) %in% Estuaries & TripType %in% DataTypes) %>%  arrange(TripID),
+                  dboTripInfo %>% filter(substring(TripID,1,2) %in% Estuaries & TripType %in% DataTypes) %>%  arrange(TripID)) %>%
+  dplyr::select(TripID:DataStatus, Comments))
+rm(hsdbTripInfo, dboTripInfo)        
+#
+(SampleEvent <- rbind(hsdbSampleEvent %>% mutate(TripDate = as.Date(substring(SampleEventID, 8, 15), format = "%Y%m%d"), FixedLocationID = substring(SampleEventID, 19, 22), DataType = substring(SampleEventID,3,6)) %>%
+                       filter(substring(SampleEventID,1,2) %in% Estuaries & TripDate > Start_date & TripDate < End_date & DataType %in% DataTypeCodes),
+                     dboSampleEvent %>% mutate(TripDate = as.Date(substring(SampleEventID, 8, 15), format = "%Y%m%d"), FixedLocationID = substring(SampleEventID, 19, 22), DataType = substring(SampleEventID,3,6)) %>%
+                       filter(substring(SampleEventID,1,2) %in% Estuaries & TripDate > Start_date & TripDate < End_date & DataType %in% DataTypeCodes)) %>%
+  dplyr::select(TripDate, DataType, SampleEventID:FixedLocationID, DataStatus, Comments))
+rm(hsdbSampleEvent, dboSampleEvent)
+#
